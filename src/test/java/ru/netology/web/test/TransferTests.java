bd651724c0291b8fbafd4b268aa7a719de82486d
BeLY4 <gleb.osin.71@mail.ru>
2025-03-07 17:26:26 +0300

    commit1

--- src/test/java/ru/netology/web/test/TransferTests.java
+++ src/test/java/ru/netology/web/test/TransferTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
